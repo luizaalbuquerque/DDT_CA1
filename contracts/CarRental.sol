fca762e05356b19b1b9c85346df1dc36cdc4e9ce
GabrielPAntoniolli <gabriel.pa16@gmail.com>
2022-11-27 09:35:24 +0000

    adding rentingACar Function

--- contracts/CarRental.sol
+++ contracts/CarRental.sol
@@ -26,4 +26,28 @@ contract CarRental {
 
         return count;
     }
+    //error to be called by next function
+    error InvalidPrice(uint correctPrice);
+
+    // function that customer calls to rent a car, the user only needs to insert the id of the car he is interested. and pay the value
+    // if the value is different from the car's price, this logic was taken from this link here:
+    // https://ethereum.stackexchange.com/questions/125092/payments-pre-set-value-in-solidity
+    function rentingACar(uint carID) public payable returns (bool){
+
+        //Making sure the customer inserts a valid ID for this to work
+        require(carID >= 0 && carID < getNumberOfCars());
+
+        // also checking if the user is paying the right amount.
+        if(msg.value != cars[carID].price)
+            revert InvalidPrice(cars[carID].price);
+
+        // with this line, we ensure that the chosen car can only be rented if available.
+        require(cars[carID].isAvailable == true);
+
+        //updating chosen car properties
+        cars[carID].customer = msg.sender;
+        cars[carID].isAvailable = false;
+
+        return true;
+    }
 }
